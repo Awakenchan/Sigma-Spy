34d812a23e048d2f4e473aa40009cf3ceb9b5e43
Awaken <93601997+Awakenchan@users.noreply.github.com>
2026-06-02 04:17:41 +0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -20,7 +20,7 @@
 --// File handling configuration 
 local FilesConfig = {
 	UseWorkspace = false,
-	RepoUrl = "https://raw.githubusercontent.com/depthso/Sigma-Spy/refs/heads/main" -- "http://127.0.0.1:3000"
+	RepoUrl = "https://github.com/Awakenchan/Sigma-Spy/tree/main" -- "http://127.0.0.1:3000"
 }
 
 --// Service handlers
