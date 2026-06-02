d5c0812e30b7ebb39178b2613b7e46a5923f3c08
Awaken <93601997+Awakenchan@users.noreply.github.com>
2026-06-02 05:21:35 +0500

    Update Hook.lua

--- lib/Hook.lua
+++ lib/Hook.lua
@@ -9,60 +9,31 @@ type table = {
 
 type MetaCallback = (Instance, ...any)->...any
 
---// Modules
 local Process
 
---// This is a custom hookmetamethod function, feel free to replace with your own
---// The callback is expected to return a nil value sometimes which should be ingored
 local function HookMetaMethod(self, Call: string, Callback: MetaCallback): MetaCallback
 	local OriginalFunc
 	OriginalFunc = hookmetamethod(self, Call, function(...)
-		--// Invoke callback and check for a reponce otherwise ignored
 		local ReturnValues = Callback(...)
 		if ReturnValues then
 			local Length = table.maxn(ReturnValues)
 			return unpack(ReturnValues, 1, Length)
 		end
 
-		--// Invoke orignal function
 		return OriginalFunc(...)
 	end)
 	return OriginalFunc
 end
 
---// Replace metatable function method, this can be a workaround on some games if hookmetamethod is detected
---// To use this, just uncomment it and comment out the method above
---//
--- local function HookMetaMethod(self, Call: string, Callback: MetaCallback): MetaCallback
--- 	local Metatable = getrawmetatable(self)
--- 	local OriginalFunc = rawget(Metatable, Call)
-	
--- 	--// Replace function
--- 	setreadonly(Metatable, false)
--- 	rawset(Metatable, Call, function(...)
--- 		--// Invoke callback and check for a reponce otherwise ignored
--- 		local ReturnValues = Callback(...)
--- 		if ReturnValues then
--- 			local Length = table.maxn(ReturnValues)
--- 			return unpack(ReturnValues, 1, Length)
--- 		end
-
--- 		return OriginalFunc(...)
--- 	end)
--- 	setreadonly(Metatable, true)
-
--- 	return OriginalFunc
--- end
-
 local function Merge(Base: table, New: table)
-	for Key, Value in next, New do
+	for Key, Value in New do
 		Base[Key] = Value
 	end
 end
 
 function Hook:RunOnActors(Code: string, ChannelId: number)
 	if not getactors then return end
-	for _, Actor in getactors() do 
+	for _, Actor in getactors() do
 		run_on_actor(Actor, Code, ChannelId)
 	end
 end
@@ -79,26 +50,24 @@ local function ProcessRemote(OriginalFunc, MetaMethod: string, self, Method: str
 end
 
 local function __IndexCallback(OriginalIndex, self, Method: string)
-	--// Check if the orignal value is a function
+	if typeof(self) ~= "Instance" then return end
+
 	local OriginalFunc = OriginalIndex(self, Method)
 	if typeof(OriginalFunc) ~= "function" then return end
 
-	--// Check if the Object is allowed 
 	if not Process:RemoteAllowed(self, "Send", Method) then return end
 
-	--// Process the remote data
-	return {function(self, ...) -- Possible detection?
+	return {function(self, ...)
 		return ProcessRemote(OriginalFunc, "__index", self, Method, ...)
 	end}
 end
 
 function Hook:HookMeta()
-	--// Namecall hook
 	local On; On = HookMetaMethod(game, "__namecall", function(self, ...)
+		if typeof(self) ~= "Instance" then return end
 		local Method = getnamecallmethod()
 		return ProcessRemote(On, "__namecall", self, Method, ...)
 	end)
-	--// Index call hook
 	local Oi; Oi = HookMetaMethod(game, "__index", function(...)
 		return __IndexCallback(Oi, ...)
 	end)
@@ -126,12 +95,12 @@ function Hook:Index(Object, Key: string)
 end
 
 function Hook:Init(Data)
-    local Modules = Data.Modules
+	local Modules = Data.Modules
 	Process = Modules.Process
 end
 
 function Hook:PushConfig(Overwrites)
-    Merge(self, Overwrites)
+	Merge(self, Overwrites)
 end
 
 function Hook:HookClientInvoke(Remote, Method, Callback): ((...any) -> ...any)?
@@ -142,40 +111,36 @@ function Hook:HookClientInvoke(Remote, Method, Callback): ((...any) -> ...any)?
 end
 
 function Hook:MultiConnect(Remotes)
-	for _, Remote in next, Remotes do
+	for _, Remote in Remotes do
 		Hook:ConnectClientRecive(Remote)
 	end
 end
 
 function Hook:ConnectClientRecive(Remote)
-	--// Check if the Remote class is allowed for receiving
 	local Allowed = Process:RemoteAllowed(Remote, "Receive")
 	if not Allowed then return end
 
-	--// Check if the Object has Remote class data
-    local ClassData = Process:GetClassData(Remote)
-    if not ClassData then return end
+	local ClassData = Process:GetClassData(Remote)
+	if not ClassData then return end
 
-    local IsRemoteFunction = ClassData.IsRemoteFunction
-    local Method = ClassData.Receive[1]
+	local IsRemoteFunction = ClassData.IsRemoteFunction
+	local Method = ClassData.Receive[1]
 	local PreviousFunction = nil
 
-	--// New callback function
 	local function Callback(...)
-        return Process:ProcessRemote({
-            Remote = Remote,
-            Method = Method,
-            OriginalFunc = PreviousFunction,
-            IsReceive = true,
-            MetaMethod = "Connect",
-            Args = {...}
-        })
+		return Process:ProcessRemote({
+			Remote = Remote,
+			Method = Method,
+			OriginalFunc = PreviousFunction,
+			IsReceive = true,
+			MetaMethod = "Connect",
+			Args = {...}
+		})
 	end
 
-	--// Connect remote
 	if not IsRemoteFunction then
-   		Remote[Method]:Connect(Callback)
-	else -- Remote functions
+		Remote[Method]:Connect(Callback)
+	else
 		pcall(function()
 			self:HookClientInvoke(Remote, Method, Callback)
 		end)
@@ -195,8 +160,7 @@ function Hook:BeginService(Libraries, ExtraData, ChannelId: number)
 			Hook = self
 		}
 	}
-	
-	--// Communication configuration
+
 	local Channel = Communication:GetChannel(ChannelId)
 	Communication:Init(InitData)
 	Communication:SetChannel(Channel)
@@ -204,13 +168,11 @@ function Hook:BeginService(Libraries, ExtraData, ChannelId: number)
 		if Type ~= "RemoteData" then return end
 		ProcessLib:SetRemoteData(Id, RemoteData)
 	end)
-	
-	--// Process configuration
+
 	ProcessLib:Init(InitData)
 	ProcessLib:SetChannelId(ChannelId)
 	ProcessLib:SetExtraData(ExtraData)
 
-	--// Hook configuration
 	self:Init(InitData)
 	self:HookMeta()
 end
